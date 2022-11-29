c9bdcadb2a0aeb4751162190baa51ddc91a89d20
Davi <davicamiloribeiro@gmail.com>
2022-11-28 23:22:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Alfa
+{
+    internal class Program
+    {
+        static void GenerateFile(string tabName, string fileName) 
+        {
+            string filePathName = "C:\temp\\" + fileName;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //var data = GenerateData();
+
+        }
+    }
+}
