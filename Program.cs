f98a533baa4903094c0e8602a8f69301212b8ff3
Davi <davicamiloribeiro@gmail.com>
2022-11-29 10:22:05 -0300

    Gerar arquivo

--- Program.cs
+++ Program.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
+using System.IO;
 
 namespace Alfa
 {
     internal class Program
     {
-        static void GenerateFile(string tabName, string fileName) 
-        {
+        static void GenerateData() { 
+        
+        }
+        static void GenerateHeader(IXLWorksheet worksheet) {
+            worksheet.Cell("A1").Value = "Teste";
+        }
+        static void GenerateFile(string tabName, string fileName){
             string filePathName = "C:\temp\\" + fileName;
+            if (File.Exists(filePathName)){ 
+                File.Delete(filePathName);
+            }
+
+            using (var workbook = new XLWorkbook()) {
+                var planilha = workbook.Worksheets.Add(tabName);
+
+                int line = 1;
+                GenerateHeader(planilha);
+                line++;
+
+                foreach (var item in lista) { 
+                    planilha.Cell("A" + line).Value = item.Value;
+                    line++;
+                }
+                workbook.SaveAs(filePathName);
+            }
 
         }
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             Console.WriteLine("Hello World!");
             //var data = GenerateData();
 
